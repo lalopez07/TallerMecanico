76c188d27e71de31bfd93123670f13333e10e2ec
lalopez07 <lucero.lopez37@unach>
2026-03-27 15:20:23 -0600

    Agregar archivos de proyecto.

---
+++ TallerMecanico/Controllers/ServiciosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TallerMecanico.Controllers
+{
+    public class ServiciosController : Controller
+    {
+        // GET: Servicios
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TallerMecanico/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TallerMecanico.MvcApplication" Language="C#" %>
